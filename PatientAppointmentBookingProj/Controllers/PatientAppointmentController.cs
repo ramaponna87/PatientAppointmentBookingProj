95004ee901a90809c2242128c5ca49eaacf2e4a4
Rama Ponna <rama.ponna@neuven.co.uk>
2023-11-09 12:11:00 +0000

    patient appointment controller dependency injection to communicate with interface

--- PatientAppointmentBookingProj/Controllers/PatientAppointmentController.cs
+++ PatientAppointmentBookingProj/Controllers/PatientAppointmentController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PatientAppointmentBookingProj.Repository.IRepository;
 
 namespace PatientAppointmentBookingProj.Controllers
 {
     public class PatientAppointmentController : Controller
     {
+        private readonly IPatientAppointmentRepository _patientdbRepository;
+
+        public PatientAppointmentController(IPatientAppointmentRepository patientdbRepository)
+        {
+            _patientdbRepository = patientdbRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
