2bae694937265ee327f462281418236d5ead4b69
Rama Ponna <rama.ponna@neuven.co.uk>
2023-11-09 11:36:45 +0000

    Get all appointments Interface implementation and dependency Injection added

--- PatientAppointmentBookingProj/Repository/PatientAppointmentRepository.cs
+++ PatientAppointmentBookingProj/Repository/PatientAppointmentRepository.cs
@@ -1,8 +1,21 @@
-﻿using PatientAppointmentBookingProj.Repository.IRepository;
+﻿using PatientAppointmentBookingProj.Data;
+using PatientAppointmentBookingProj.Models.ViewModel;
+using PatientAppointmentBookingProj.Repository.IRepository;
 
 namespace PatientAppointmentBookingProj.Repository
 {
     public class PatientAppointmentRepository : IPatientAppointmentRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public PatientAppointmentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public Task<IEnumerable<PatientAppointmentVM>> GetAllAppointments()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
