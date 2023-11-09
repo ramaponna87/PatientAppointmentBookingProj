79bf5339c3f2fa3aff267ddf2aebda5c9df3f2c0
Rama Ponna <rama.ponna@neuven.co.uk>
2023-11-09 11:34:19 +0000

    PatientAppointmentVM and interface declaration added

---
+++ PatientAppointmentBookingProj/Models/ViewModel/PatientAppointmentVM.cs
@@ -0,0 +1,19 @@
+﻿namespace PatientAppointmentBookingProj.Models.ViewModel
+{
+    public class PatientAppointmentVM
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+
+        public string Issue { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public bool isApproved { get; set; }
+
+
+    }
+}
--- PatientAppointmentBookingProj/Repository/IRepository/IPatientAppointmentRepository.cs
+++ PatientAppointmentBookingProj/Repository/IRepository/IPatientAppointmentRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace PatientAppointmentBookingProj.Repository.IRepository
+﻿using PatientAppointmentBookingProj.Models.ViewModel;
+
+namespace PatientAppointmentBookingProj.Repository.IRepository
 {
     public interface IPatientAppointmentRepository
     {
+        Task<IEnumerable<PatientAppointmentVM>> GetAllAppointments();
+
     }
 }
