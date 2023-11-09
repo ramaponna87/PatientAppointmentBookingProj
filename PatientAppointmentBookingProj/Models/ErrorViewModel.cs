2c3480cc135eb020c4fc8b2eeaf1f8ca66870b20
Rama Ponna <rama.ponna@neuven.co.uk>
2023-11-09 09:45:44 +0000

    Add project files.

---
+++ PatientAppointmentBookingProj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PatientAppointmentBookingProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
